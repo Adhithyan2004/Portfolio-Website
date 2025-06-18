4735c7dc098e5f7bf9a529f1e520d4d844f48a35
Adhithyan2004 <adhibanugopan@gmail.com>
2025-06-18 20:44:37 +0530

    change in contact suheading

--- src/components/ContCnt.jsx
+++ src/components/ContCnt.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 const ContCnt = () => {
   return (
-    <div className="CntCntr mx-16 ">
+    <div className="mx-16 CntCntr ">
       <h1 className="CntTtl text-[30vh] text-secondary w-[180vh] leading-[22vh] my-20 mb-20">
         Let's <span className="text-black">work</span> together{" "}
       </h1>
       <p className="CntPara text-5xl text-secondary w-[140vh] my-8">
-        If you're <span className="text-black">interested</span> in discussing
-        potential opportunities or collaborations, please don't hesitate to
-        reach out <span className="text-black">via email.</span>
+        Feel free to reach out via{" "}
+        <span className="text-blk">email—always</span> happy to connect or chat
+        about <span className="text-blk">interesting ideas.</span>
       </p>
-      <div className="CntWays my-10">
-        <p className="Cntmail text-secondary text-2xl">EMAIL</p>
-        <p className="Cntml text-4xl">adhibanugopan@gmail.com</p>
+      <div className="my-10 CntWays">
+        <p className="text-2xl Cntmail text-secondary">EMAIL</p>
+        <p className="text-4xl Cntml">adhibanugopan@gmail.com</p>
       </div>
-      <div className="CntWays my-10">
-        <p className="Cntphn text-secondary text-2xl">PHONE</p>
-        <p className="Cntph text-4xl">+91 6380274793</p>
+      <div className="my-10 CntWays">
+        <p className="text-2xl Cntphn text-secondary">PHONE</p>
+        <p className="text-4xl Cntph">+91 6380274793</p>
       </div>
     </div>
   );
